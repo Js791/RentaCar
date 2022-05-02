325ae0cf2613768c896be2579f373c4e668620e5
Лев <90228698+Js791@users.noreply.github.com>
2022-05-01 20:44:23 -0400

    depolyment purposes

--- public_html/rent.php
+++ public_html/rent.php
@@ -3,7 +3,7 @@ include_once "../lib/functions.php";
 session_start();
 if (!is_logged_in()) {
     flash("You must be logged on to view this page", "warning");
-    redirect("login.php");
+    redirect("login.php"); //for branching
 }
 ?>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
